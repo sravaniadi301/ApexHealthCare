a7c09c7e757173ef359dc528514b5c0c5a68c07f
User <User@Admin>
2024-08-12 16:31:47 +0530

    My first commit

---
+++ ApexHealthCareHRMSFramework/src/main/java/com/apexhealthcare/hrms/objectrepositoryutility/LoginPage.java
@@ -0,0 +1,46 @@
+package com.apexhealthcare.hrms.objectrepositoryutility;
+
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+
+import BaseTestUtility.BaseClass;
+
+//Rule1:Create a separate java class
+public class LoginPage extends BaseClass
+{
+	
+   WebDriver driver;
+	public LoginPage(WebDriver driver)
+	{
+		this.driver=driver;
+		PageFactory.initElements(driver,this);
+	}
+    
+  //Rule2:Object creation
+	
+	@FindBy(xpath="(//a[text()='Click Here'])[3]")
+	private WebElement clickhere;
+	
+	
+
+
+	public WebElement getClickhere() {
+		return clickhere;
+	}
+
+	//Rule 5:provide Action
+	public void loginToapp(String username,String password) {
+		
+		driver.manage().window().maximize();
+		
+		clickhere.click();
+	}
+	
+	
+	
+}
---
+++ ApexHealthCareHRMSFramework/src/test/java/Apex/HrmsLogin/LaunchUrl.java
@@ -0,0 +1,27 @@
+package Apex.HrmsLogin;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+import com.apexhealthcare.hrms.objectrepositoryutility.LoginPage;
+
+
+import BaseTestUtility.BaseClass;
+
+public class LaunchUrl extends BaseClass{
+	@Test
+	public void launchurl() {
+	 
+		driver.manage().window().maximize();
+	
+  LoginPage cnp=new LoginPage(driver);
+	cnp.getClickhere().click();
+   
+    driver.close();
+    
+}
+
+
+
+}
