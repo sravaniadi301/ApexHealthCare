0ecb22510236e7f075c669527d84a8029efe6916
User <User@Admin>
2024-08-13 11:30:26 +0530

    my 3rd commit

--- sampletest1/src/test/java/com/sample/gitpack/sampleclass1.java
+++ sampletest1/src/test/java/com/sample/gitpack/sampleclass1.java
@@ -1,10 +1,14 @@
 package com.sample.gitpack;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class sampleclass1 {
 
 	public static void main(String[] args) {
 		System.out.println("sample project");
-		driver.get("http://49.249.28.218:8081/AppServer/Hospital_Management_System")
+		 WebDriver driver=new ChromeDriver();
+		driver.get("http://49.249.28.218:8081/AppServer/Hospital_Management_System");
 
 	}
 
