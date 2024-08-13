f9fc49b7820f41239aa623071aa989accbbcfafa
sravaniadi301 <60929693+sravaniadi301@users.noreply.github.com>
2024-08-13 11:53:49 +0530

    Update sampleclass12.java

--- sampletest1/src/test/java/com/sample/gitpack/sampleclass12.java
+++ sampletest1/src/test/java/com/sample/gitpack/sampleclass12.java
@@ -5,7 +5,7 @@ public class sampleclass12 {
 	public static void main(String[] args) {
 	System.out.println("hello");
 	System.out.println("hii....");
-
+        System.out.println("Finish...");
 	}
 
 }
