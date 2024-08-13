4615b239b9d35078fce8e02c38d74242c88c5f3a
sravaniadi301 <60929693+sravaniadi301@users.noreply.github.com>
2024-08-13 11:27:39 +0530

    Update sampleclass1.java

--- sampletest1/src/test/java/com/sample/gitpack/sampleclass1.java
+++ sampletest1/src/test/java/com/sample/gitpack/sampleclass1.java
@@ -4,6 +4,7 @@ public class sampleclass1 {
 
 	public static void main(String[] args) {
 		System.out.println("sample project");
+		driver.get("http://49.249.28.218:8081/AppServer/Hospital_Management_System")
 
 	}
 
