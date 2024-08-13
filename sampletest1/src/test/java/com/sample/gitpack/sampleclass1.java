f4add15704d445736f452fa2b54e28c1beaac05c
sravaniadi301 <60929693+sravaniadi301@users.noreply.github.com>
2024-08-13 14:46:21 +0530

    Update sampleclass1.java

--- sampletest1/src/test/java/com/sample/gitpack/sampleclass1.java
+++ sampletest1/src/test/java/com/sample/gitpack/sampleclass1.java
@@ -7,9 +7,7 @@ public class sampleclass1 {
 
 	public static void main(String[] args) {
 		System.out.println("sample project");
-		 WebDriver driver=new ChromeDriver();
-		driver.get("http://49.249.28.218:8081/AppServer/Hospital_Management_System");
-
+		System.out.println("sample project pull request"); 
 	}
 
 }
