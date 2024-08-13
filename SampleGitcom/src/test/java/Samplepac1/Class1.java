47d752ca49faba4bf551e1a2f5e67ebb6be0acd7
User <User@Admin>
2024-08-13 13:28:10 +0530

    My 1st commit

---
+++ SampleGitcom/src/test/java/Samplepac1/Class1.java
@@ -0,0 +1,10 @@
+package Samplepac1;
+
+public class Class1 {
+
+	public static void main(String[] args) {
+		System.out.println("hello");
+
+	}
+
+}
---
+++ SampleGitcom/src/test/java/Samplepac2/Class2.java
@@ -0,0 +1,10 @@
+package Samplepac2;
+
+public class Class2 {
+
+	public static void main(String[] args) {
+		System.out.println("hi....");
+
+	}
+
+}
