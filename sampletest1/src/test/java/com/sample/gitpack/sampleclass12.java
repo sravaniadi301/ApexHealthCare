7fd55aa020a7dab572e88564fbea6c6e4f35cae2
User <User@Admin>
2024-08-13 11:51:10 +0530

    My 4th commit

--- sampletest1/src/test/java/com/sample/gitpack/sampleclass12.java
+++ sampletest1/src/test/java/com/sample/gitpack/sampleclass12.java
@@ -3,7 +3,8 @@ package com.sample.gitpack;
 public class sampleclass12 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	System.out.println("hello");
+	System.out.println("hii....");
 
 	}
 
